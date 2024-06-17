b60513e76bc9a136fa2b2070dbd32567375d1064
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-06-17 15:33:47 +0530

    raw string

--- example.dart
+++ example.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 
 void main()
 {
-    stdout.writeln('Type in your name please');
-    String? input = stdin.readLineSync();
-
-    stdout.writeln('Hi $input nice to meet u ');
+    var a=r'hello \n world';
+    print(a);
 }
